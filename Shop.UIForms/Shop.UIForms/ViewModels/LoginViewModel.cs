fc91be30169ee8f76b6fd2be2058674ec6d71375
camilo <camivalo01@gmail.com>
2019-06-14 16:21:32 -0500

    Show product on Xamarin Forms

--- Shop.UIForms/Shop.UIForms/ViewModels/LoginViewModel.cs
+++ Shop.UIForms/Shop.UIForms/ViewModels/LoginViewModel.cs
@@ -1,10 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Shop.UIForms.ViewModels
+﻿namespace Shop.UIForms.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
+    using Shop.UIForms.Views;
+    using System;
+    using System.Windows.Input;
+    using Xamarin.Forms;
+
     public class LoginViewModel
     {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public ICommand LoginCommand => new RelayCommand(Login);
+
+        public LoginViewModel()
+        {
+            this.Email = "camivalo01@gmail.com";
+            this.Password = "123456";
+        }
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an email",
+                    "Accept");
+                return;
+            }
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an password",
+                    "Accept");
+                return;
+            }
+            if(!this.Email.Equals("camivalo01@gmail.com") || !this.Password.Equals("123456"))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                   "Error",
+                   "User or Password wrong",
+                   "Accept");
+                return;
+            }
+            //await Application.Current.MainPage.DisplayAlert(
+            //        "OK",
+            //        "CELENTE MIJO",
+            //        "Accept");
+
+            MainViewModel.GetInstance().Products = new ProductsViewModel();
+            await Application.Current.MainPage.Navigation.PushAsync(new ProductsPage());
+        }
     }
 }
---
+++ Shop.UIForms/Shop.UIForms/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,71 @@
+﻿namespace Shop.UIForms.ViewModels
+{
+    using GalaSoft.MvvmLight.Command;
+    using Shop.Common.Models;
+    using Shop.Common.Services;
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Windows.Input;
+    using Xamarin.Forms;
+
+
+    public class ProductsViewModel : BaseViewModel
+    {
+        private readonly ApiService apiService;
+        private ObservableCollection<Product> products;
+        private bool isRefreshing;
+
+        //public ICommand SeparateCommand => new RelayCommand(Separate);
+
+        //private async void Separate()
+        //{
+        //    await Application.Current.MainPage.DisplayAlert(
+        //          "Error",
+        //          "You must enter an email",
+        //          "Accept");
+        //}
+
+        public ObservableCollection<Product> Products
+        {
+            get => this.products;
+            set => this.SetValue(ref this.products, value);
+        }
+
+        public bool IsRefreshing
+        {
+            get => this.isRefreshing;
+            set => this.SetValue(ref this.isRefreshing, value);
+        }
+
+        public ProductsViewModel()
+        {
+            this.apiService = new ApiService();
+            this.LoadProducts();
+        }
+
+        private async void LoadProducts()
+        {
+            this.IsRefreshing = true;
+
+            var response = await this.apiService.GetListAsync<Product>(
+                "https://shopcolchonesplacer.azurewebsites.net",
+                "/api",
+                "/Products");
+
+            this.IsRefreshing = false;
+
+            if (!response.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error!!",
+                    response.Message,
+                    "Accept");
+                return;
+            }
+
+            var myProducts = (List<Product>)response.Result;
+            this.Products = new ObservableCollection<Product>(myProducts);
+        }
+    }
+}
